53485e515b1c7aff6183a725b0e04775be9a6215
shubham101096 <shubhammishra10@gmail.com>
2021-06-24 09:38:59 +0530

    fix letter generated only for first name in the list

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 with open("Input/Names/invited_names.txt") as file:
     names = file.readlines()
 
-with open("Input/Letters/starting_letter.txt") as starting_letter:
-    for name in names:
+print(names)
+
+for name in names:
+    with open("Input/Letters/starting_letter.txt") as starting_letter:
         letter = starting_letter.read()
+        print(name)
         new_letter = letter.replace("[name]", name.strip())
+        print(letter)
+        print(new_letter)
         with open(f"Output/ReadyToSend/letterfor{name.strip()}.txt", "w") as file:
             file.write(new_letter)
-
-
-
